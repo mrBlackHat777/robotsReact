51b8dff1cdede335d7bb8b52ecd6f8ee83436e75
Mohamed ILias <ilias.mohamed@outlook.com>
2020-04-28 12:37:35 -0400

    Updates

---
+++ precache-manifest.79c26bfc2faf6148a80d0cdd0d211b88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6990e4ca78a3cd962ac2a290cfb3c684",
+    "url": "/robotsReact/index.html"
+  },
+  {
+    "revision": "7596fe790a7f455ca76f",
+    "url": "/robotsReact/static/css/main.d09c1630.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/robotsReact/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotsReact/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7596fe790a7f455ca76f",
+    "url": "/robotsReact/static/js/main.df347436.chunk.js"
+  },
+  {
+    "revision": "7bae232bba414b6be1e2",
+    "url": "/robotsReact/static/js/runtime-main.6f699802.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robotsReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
